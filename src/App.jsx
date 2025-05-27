7e8288c39db0ff6e607888130e843abd4bda3807
Sophia202407 <sophiawanghongxia@gmail.com>
2025-05-27 21:04:21 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { BrowserRouter } from 'react-router-dom'
+import './App.css'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <h1>Welcome to my Capstone Project!</h1>
+    </BrowserRouter>
+  )
+}
+
+export default App
